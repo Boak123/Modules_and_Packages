ee76534871ff013cc1db7489daf3571f99c716bc
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-17 18:51:37 -0700

    updated sys module

--- name.py
+++ name.py
@@ -1,4 +1,9 @@
 import sys
 
 name = sys.argv[1]
-print(f"Hello, {name}!")
+if len(sys.argv) > 2:
+    print("Too many arguments provided. Please provide only your name. ")
+elif len(sys.argv) < 2:
+    print("Too few arguments provided. Please provide your name as an argument.")
+else:
+    print(f"Welcome, {name}! Let's get started.")
