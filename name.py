afb963e5cb431a4c6c345f32552d51189096d53c
Boak123 <boluwatifeakorede17@gmail.com>
2026-07-17 19:03:41 -0700

    updated and added sys.exit to give out better error message

--- name.py
+++ name.py
@@ -1,9 +1,9 @@
 import sys
 
-name = sys.argv[1]
 if len(sys.argv) > 2:
-    print("Too many arguments provided. Please provide only your name. ")
+    sys.exit("Too many arguments provided. Please provide only your name. ")
 elif len(sys.argv) < 2:
-    print("Too few arguments provided. Please provide your name as an argument.")
+    sys.exit("Too few arguments provided. Please provide your name as an argument.")
 else:
+    name = sys.argv[1]
     print(f"Welcome, {name}! Let's get started.")
